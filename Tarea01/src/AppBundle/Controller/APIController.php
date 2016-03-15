a0653cc22e1d843eedcebdb32bfeab47bf464264
TristanPouliquen <tristan.pouliquen@yahoo.fr>
2016-03-15 17:46:25 -0300

    Added missing use statement

--- Tarea01/src/AppBundle/Controller/APIController.php
+++ Tarea01/src/AppBundle/Controller/APIController.php
@@ -6,6 +6,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 class APIController extends Controller
